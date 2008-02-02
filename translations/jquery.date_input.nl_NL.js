13e75a0190110030a7f7d7ea9c336a116918a405
Jon Leighton <j@jonathanleighton.com>
2008-02-02 15:40:15 +0000

    Dutch translation [Edwin Martin]

---
+++ translations/jquery.date_input.nl_NL.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"],
+  short_month_names: ["Jan", "Feb", "Maa", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+  short_day_names: ["Zo", "Ma", "Di", "Wo", "Do", "Vr", "Za"]
+});
