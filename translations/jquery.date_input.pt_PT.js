cf8d7f179499366ff84913a8ddd0407798020ba2
Jon Leighton <j@jonathanleighton.com>
2008-02-06 17:39:26 +0000

    Portuguese translation [Jean Reis]

---
+++ translations/jquery.date_input.pt_PT.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+  short_month_names: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+  short_day_names: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"]
+});
