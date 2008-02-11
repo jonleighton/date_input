9790b2bbf3b2c3c3cb44d7c21599ab6c86edc66e
Jon Leighton <j@jonathanleighton.com>
2008-02-11 13:32:36 +0000

    German translation [Stefan Rado]

---
+++ translations/jquery.date_input.de_DE.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+  short_month_names: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+  short_day_names: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+});
