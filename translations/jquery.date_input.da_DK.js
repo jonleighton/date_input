1cc7f80a1bf4b7e0568bf3befda9cd79decd3a57
Jon Leighton <j@jonathanleighton.com>
2008-02-17 17:43:57 +0000

    Danish translation [Jan Christensen]

---
+++ translations/jquery.date_input.da_DK.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli", "August", "September", "Oktober", "November", "December"],
+  short_month_names: ["Jan", "Feb", "Mar", "Apr", "Maj", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+  short_day_names: ["Sø", "Ma", "Ti", "On", "To", "Fr", "Lø"]
+});
