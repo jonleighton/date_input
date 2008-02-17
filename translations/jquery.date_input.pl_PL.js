775de1b2ffdedb03f59979e47fc9c152a43862ba
Jon Leighton <j@jonathanleighton.com>
2008-02-17 17:46:04 +0000

    Polish [Adam Kozubowicz]

---
+++ translations/jquery.date_input.pl_PL.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"],
+  short_month_names: ["Sty", "Lut", "Mar", "Kwi", "Maj", "Cze", "Lip", "Sie", "Wrz", "Paź", "Lis", "Gru"],
+  short_day_names: ["Nie", "Pon", "Wto", "Śro", "Czw", "Pią", "Sob"]
+});
