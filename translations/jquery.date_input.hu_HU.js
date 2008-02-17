1642acf3d61690349881fa6629b6f4c118eb6cd4
Jon Leighton <j@jonathanleighton.com>
2008-02-17 17:47:25 +0000

    Hungarian [Horváth Balázs]

---
+++ translations/jquery.date_input.hu_HU.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"],
+  short_month_names: ["Jan", "Febr", "Márc", "Ápr", "Máj", "Jún", "Júl", "Aug", "Szept", "Okt", "Nov", "Dec"],
+  short_day_names: ["V", "H", "K", "SZ", "CS", "P", "SZ"]
+});
