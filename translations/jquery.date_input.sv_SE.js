316d42a4dde150a79ee361add45d4fcfcca97cb7
Jon Leighton <j@jonathanleighton.com>
2008-02-17 17:56:56 +0000

    Swedish [Christian Jarhult]

---
+++ translations/jquery.date_input.sv_SE.js
@@ -0,0 +1,5 @@
+$.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Januari", "Februari", "Mars", "April", "Maj", "Juni", "Juli", "Augusti", "September", "Oktober", "November", "December"],
+  short_month_names: ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+  short_day_names: ["sön", "mån", "tis", "ons", "tor", "fre", "lör"]
+});
