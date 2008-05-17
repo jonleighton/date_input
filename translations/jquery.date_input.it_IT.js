96de75fb4a48a5cab0421f8acd438280a23503c9
Jon Leighton <j@jonathanleighton.com>
2008-05-17 17:52:55 +0000

    Italian translation (#2230)

---
+++ translations/jquery.date_input.it_IT.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+  short_month_names: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
+  short_day_names: ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"]
+});
