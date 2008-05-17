10b388a65a95c2745834acbe10c665f70dda45ff
Jon Leighton <j@jonathanleighton.com>
2008-05-17 17:55:35 +0000

    Indonesian translation (#2332)

---
+++ translations/jquery.date_input.id_ID.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+  short_month_names: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Agu", "Sep", "Okt", "Nov", "Des"],
+  short_day_names: ["Min", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"]
+});
