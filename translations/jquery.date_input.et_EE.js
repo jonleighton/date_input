860cd8a7fcb0ec9d0aff93ab433f480792b71887
Jon Leighton <j@jonathanleighton.com>
2008-05-17 17:59:40 +0000

    Estonian translation (#2340)

---
+++ translations/jquery.date_input.et_EE.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+	month_names: ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"],
+	short_month_names: ["jaan", "veebr", "märts", "apr", "mai", "juuni", "juuli", "aug", "sept", "okt", "nov", "dets"],
+	short_day_names: ["P", "E", "T", "K", "N", "R", "L"]
+});
