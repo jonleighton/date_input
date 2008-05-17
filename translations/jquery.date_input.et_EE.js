38716f15076b38f66b2b32374dcc6f72488acb2d
Jon Leighton <j@jonathanleighton.com>
2008-05-17 18:01:52 +0000

    Fix hard tabs

--- translations/jquery.date_input.et_EE.js
+++ translations/jquery.date_input.et_EE.js
@@ -1,5 +1,5 @@
 jQuery.extend(DateInput.DEFAULT_OPTS, {
-	month_names: ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"],
-	short_month_names: ["jaan", "veebr", "märts", "apr", "mai", "juuni", "juuli", "aug", "sept", "okt", "nov", "dets"],
-	short_day_names: ["P", "E", "T", "K", "N", "R", "L"]
+  month_names: ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"],
+  short_month_names: ["jaan", "veebr", "märts", "apr", "mai", "juuni", "juuli", "aug", "sept", "okt", "nov", "dets"],
+  short_day_names: ["P", "E", "T", "K", "N", "R", "L"]
 });
