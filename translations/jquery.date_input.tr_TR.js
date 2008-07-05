f247fea555bc47695344976e5c10da2d158f9cb9
Jon Leighton <j@jonathanleighton.com>
2008-07-05 19:53:10 +0000

    Turkish translation

---
+++ translations/jquery.date_input.tr_TR.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Ocak", "Şubat", "Mart", "Nisanl", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+  short_month_names: ["Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağu", "Eyl", "Ekim", "Kas", "Ara"],
+  short_day_names: ["Pzr", "Pzt", "Sal", "Çrş", "Prş", "Cum", "Cmt"]
+});
