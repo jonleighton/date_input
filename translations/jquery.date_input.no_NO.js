0459a14a9ee9c081d244d0eee4ea39d35d97880a
Jon Leighton <j@jonathanleighton.com>
2008-07-05 20:01:52 +0000

    Norwegian translation

---
+++ translations/jquery.date_input.no_NO.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Janaur", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"],
+  short_month_names: ["jan", "feb", "mar", "apr", "mai", "jun", "jul", "aug", "sep", "okt", "nov", "des"],
+  short_day_names: ["sø","ma","ti", "on", "to", "fr", "lø"]
+});
