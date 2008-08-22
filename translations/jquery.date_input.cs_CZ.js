4c85a542def64d691605e73cf1e9a4d306744c68
Jon Leighton <j@jonathanleighton.com>
2008-08-22 20:26:20 +0000

    Czech translation

---
+++ translations/jquery.date_input.cs_CZ.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Leden", "Únor", "Březen", "Duben", "Květen", "Červen", "Červenec", "Srpen", "Září", "Říjen", "Listopad", "Prosinec"],
+  short_month_names: ["Led", "Úno", "Bře", "Dub", "Kvě", "Čer", "Čvc", "Srp", "Zář", "Říj", "Lis", "Pro"],
+  short_day_names: ["Ne", "Po", "Út", "St", "Čt", "Pá", "So"]
+});
