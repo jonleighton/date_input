2e72a82f1c541d97af2de26dfe2d48de6a4bf8ea
Jon Leighton <j@jonathanleighton.com>
2008-08-22 20:29:57 +0000

    Slovak translation

---
+++ translations/jquery.date_input.sk_SK.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Január", "Február", "Marec", "Apríl", "Máj", "Jún", "Júl", "August", "September", "Október", "November", "December"],
+  short_month_names: ["Jan", "Feb", "Mar", "Apr", "Máj", "Jún", "Júl", "Aug", "Sep", "Okt", "Nov", "Dec"],
+  short_day_names: ["Ned", "Pon", "Uto", "Str", "Štv", "Pia", "Sob"]
+});
