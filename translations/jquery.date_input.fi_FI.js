9a9c077a54f42ff439fb860252d93adaa9b43190
Matias Korhonen <matias@kiskolabs.com>
2011-04-15 20:04:38 +0300

    Adds Finnish localisation.

---
+++ translations/jquery.date_input.fi_FI.js
@@ -0,0 +1,5 @@
+jQuery.extend(DateInput.DEFAULT_OPTS, {
+  month_names: ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"],
+  short_month_names: ["Tammi", "Helmi", "Maalis", "Huhti", "Touko", "Kesä", "Heinä", "Elo", "Syys", "Loka", "Marras", "Joulu"],
+  short_day_names: ["Ma", "Ti", "Ke", "To", "Pe", "La", "Su"]
+});
