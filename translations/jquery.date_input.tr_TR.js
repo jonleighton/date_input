3a55e923e521cf1f118463311b58509cc59107f7
Kadir Doğan <kadir.doggan@gmail.com>
2012-09-13 21:09:22 +0300

    Update translations/jquery.date_input.tr_TR.js
    fixed misspelling

--- translations/jquery.date_input.tr_TR.js
+++ translations/jquery.date_input.tr_TR.js
@@ -1,5 +1,5 @@
 jQuery.extend(DateInput.DEFAULT_OPTS, {
-  month_names: ["Ocak", "Şubat", "Mart", "Nisanl", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+  month_names: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
   short_month_names: ["Oca", "Şub", "Mar", "Nis", "May", "Haz", "Tem", "Ağu", "Eyl", "Ekim", "Kas", "Ara"],
   short_day_names: ["Pzr", "Pzt", "Sal", "Çrş", "Prş", "Cum", "Cmt"]
 });
